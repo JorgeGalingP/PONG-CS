90533ae3392f2f6fa9641c5d0041ad3487d86c81
Jorge0594 <jsm0594@gmail.com>
2017-10-13 20:10:35 +0200

    Bounce ball working

--- PONG/app/src/main/java/com/example/jorge/myapplication/MainActivity.java
+++ PONG/app/src/main/java/com/example/jorge/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.jorge.myapplication;
 
-import android.graphics.Path;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,13 +11,13 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView player1, playerCom, ball;
+    private ImageView player1, playerCom, ball, net;
     private Boolean down, up, changeDirection;
     private Handler controlCom = new Handler();
     private Handler controlBall = new Handler();
 
     private int direction;
-    private final int speedMove = 50;
+    private final int speedMove = 10;
     private final int speedBall = 10;
     private RelativeLayout board;
 
@@ -30,11 +29,13 @@ public class MainActivity extends AppCompatActivity {
         player1 = (ImageView) findViewById(R.id.player1);
         playerCom = (ImageView) findViewById(R.id.comPlayer);
         ball = (ImageView) findViewById(R.id.ball);
+        net = (ImageView) findViewById(R.id.net);
         board = (RelativeLayout) findViewById(R.id.board);
         down = false;
         up = false;
         changeDirection = false;
 
+        net.setMaxHeight(board.getHeight());
 
         playerComMovements.run();
         ballMovements.run();
@@ -94,59 +95,139 @@ public class MainActivity extends AppCompatActivity {
             } else if ((!up && down) && (!((player1.getY() + player1.getHeight() + speedMove) >= (board.getHeight())))) {
                 player1.setY(player1.getY() + speedMove);
             }
-            controlCom.postDelayed(playerComMovements, 100);
+            controlCom.postDelayed(playerComMovements, (long) 0.005);
         }
     };
 
     Runnable ballMovements = new Runnable() {
         @Override
         public void run() {
-
-            if ((ball.getX() < 0) || (ball.getX() > board.getWidth())) {
+            boolean collision = detectBallCollisionPaddle(ball);
+            System.out.println(collision);
+            if (((ball.getX() < 0) || (ball.getX() > board.getWidth())) && (!collision)) {
+                System.out.println("REINICIO");
                 ball.setX(board.getWidth() / 2);
                 ball.setY(board.getHeight() / 2);
                 direction = 0;
-                /*try {
-                    controlBall.wait(300);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }*/
             } else {
-                if (((player1.getY() <= ball.getY()) && ((player1.getY() + player1.getHeight()) >= (ball.getY()))) && ((player1.getX() + player1.getWidth()) >= ball.getX())&&(direction == 0)) {
-                    direction = 2;
-                } else {
-                    if (((ball.getY() + speedBall) <= 0) && (direction == 0)) {
-                        direction = 1;
-                    } else if (direction == 2) {
-                        direction = 3;
+                if (collision) {
+                    switch (direction) {
+                        case 0:
+                            direction = 2;
+                            break;
+                        case 1:
+                            direction = 3;
+                            break;
+                        case 2:
+                            direction = 0;
+                            break;
+                        case 3:
+                            direction = 1;
+                            break;
                     }
+                }
 
-                    if (((ball.getY() + ball.getHeight()) + speedMove >= board.getHeight()) && (direction == 1)) {
-                        direction = 0;
-                    } else if (direction == 3) {
-                        direction = 2;
-                    }
+                if (((ball.getY() + speedBall) <= 0) && (direction == 0)) {
+                    direction = 1;
+                } else if (((ball.getY() + speedBall) <= 0) && (direction == 2)) {
+                    direction = 3;
                 }
-                switch (direction) {//rebotes
-                    case 0: //arriba izquierda
-                        ball.setY(ball.getY() - speedBall);
-                        ball.setX(ball.getX() - speedBall);
-                        break;
-                    case 1: //abajo izquierda
-                        ball.setY(ball.getY() + speedBall);
-                        ball.setX(ball.getX() - speedBall);
-                        break;
-                    case 2://arriba derecha
-                        ball.setY(ball.getY() - speedBall);
-                        ball.setX(ball.getX() + speedBall);
-                        break;
-                    case 3: //abajo derecha
-                        ball.setY(ball.getY() + speedBall);
-                        ball.setX(ball.getX() + speedBall);
-                        break;
+
+                if (((ball.getY() + ball.getHeight()) + speedMove >= board.getHeight()) && (direction == 1)) {
+                    direction = 0;
+                } else if (((ball.getY() + ball.getHeight()) + speedMove >= board.getHeight()) && (direction == 3)) {
+                    direction = 2;
                 }
+                System.out.println("LA DIRECCION SEGUNDO INTENTO ES " + direction);
+                ballMovements(ball);
             }
             controlBall.postDelayed(ballMovements, (long) 0.005);
         }
+
+
+        public void changeBallDirectionByPaddle(int dir) {
+            switch (dir) {
+                case 0:
+                    dir = 2;
+                    break;
+                case 1:
+                    dir = 3;
+                    break;
+                case 2:
+                    dir = 0;
+                    break;
+                case 3:
+                    dir = 1;
+                    break;
+            }
+        }
     };
+
+    public boolean detectBallCollisionPaddle(ImageView ball) {
+        //Collision player1
+        if (((ball.getY() >= player1.getY()) ||
+                ((ball.getY() + ball.getHeight()) >= player1.getY())) &&
+                ((ball.getY() <= (player1.getY() + player1.getHeight())) ||
+                        ((ball.getY() + ball.getHeight()) <= (player1.getY() + player1.getHeight())))) {
+            if (((ball.getX() - speedBall) <= 0)) {
+                ball.setX(player1.getX()+player1.getWidth());
+                return true;
+            } else if ((ball.getX() <= (player1.getX() + player1.getWidth()))) {
+                ball.setX(player1.getX()+player1.getWidth());
+                return true;
+            }
+        }
+        //Collision comPlayer 
+        if (((ball.getY() >= playerCom.getY()) ||
+                ((ball.getY() + ball.getHeight()) >= playerCom.getY())) &&
+                ((ball.getY() <= (playerCom.getY() + playerCom.getHeight())) ||
+                        ((ball.getY() + ball.getHeight()) <= (playerCom.getY() + playerCom.getHeight())))) {
+            if (((ball.getX() + speedBall) >= board.getWidth())) {
+                ball.setX(playerCom.getX()-ball.getWidth());
+                return true;
+            } else if (((ball.getX()+ball.getWidth()) >= playerCom.getX())) {
+                ball.setX(playerCom.getX()-ball.getWidth());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void ballMovements(ImageView ball) {
+        switch (direction) {//rebotes
+            case 0: //arriba izquierda
+                ball.setY(ball.getY() - speedBall);
+                ball.setX(ball.getX() - speedBall);
+                break;
+            case 1: //abajo izquierda
+                ball.setY(ball.getY() + speedBall);
+                ball.setX(ball.getX() - speedBall);
+                break;
+            case 2://arriba derecha
+                ball.setY(ball.getY() - speedBall);
+                ball.setX(ball.getX() + speedBall);
+                break;
+            case 3: //abajo derecha
+                ball.setY(ball.getY() + speedBall);
+                ball.setX(ball.getX() + speedBall);
+                break;
+        }
+    }
+
+    public void changeBallDirection(int dir) {
+        switch (dir) {
+            case 0:
+                dir = 2;
+                break;
+            case 1:
+                dir = 3;
+                break;
+            case 2:
+                dir = 0;
+                break;
+            case 3:
+                dir = 1;
+                break;
+        }
+    }
 }
