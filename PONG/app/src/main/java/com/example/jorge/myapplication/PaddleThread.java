954b4e0a675e494f5d2b17e4783553e52f8d2f7b
Jorge0594 <jsm0594@gmail.com>
2017-10-22 21:37:09 +0200

    New paddle & ball movements

---
+++ PONG/app/src/main/java/com/example/jorge/myapplication/PaddleThread.java
@@ -0,0 +1,123 @@
+package com.example.jorge.myapplication;
+
+
+import android.provider.Settings;
+import android.widget.ImageView;
+
+import android.os.Handler;
+
+/**
+ * Created by Jorge on 20/10/2017.
+ */
+
+public class PaddleThread implements Runnable {
+    private boolean changeDirection;
+    private Handler control;
+    private int height,width, speedMove, direction;
+    private ImageView paddle;
+    MainActivity main;
+
+    public PaddleThread(MainActivity main) {
+        control = new Handler();
+        changeDirection = false;
+        this.main = main;
+        speedMove = 10;
+    }
+
+    @Override
+    public void run() {
+        paddle = main.getPaddle();
+        height = main.getBoard().getHeight();
+        width = main.getBoard().getWidth();
+        direction = changeDirections(direction);
+        paddleMovements(paddle);
+        control.postDelayed(this,(long)0.005);
+    }
+
+    public void paddleMovements(ImageView paddle) {
+        switch (direction) {//rebotes
+            case 0: //arriba izquierda
+                paddle.setY(paddle.getY() - speedMove);
+                paddle.setX(paddle.getX() - speedMove);
+                break;
+            case 1: //abajo izquierda
+                paddle.setY(paddle.getY() + speedMove);
+                paddle.setX(paddle.getX() - speedMove);
+                break;
+            case 2://arriba derecha
+                paddle.setY(paddle.getY() - speedMove);
+                paddle.setX(paddle.getX() + speedMove);
+                break;
+            case 3: //abajo derecha
+                paddle.setY(paddle.getY() + speedMove);
+                paddle.setX(paddle.getX() + speedMove);
+                break;
+        }
+    }
+
+    public int changeDirections (int direction){
+        if (((paddle.getY() + speedMove) <= 0) && (direction == 0)) {
+            return 1;
+        } else if (((paddle.getY() + speedMove) <= 0) && (direction == 2)) {
+            return 3;
+        }
+
+        if((paddle.getX()-speedMove <= 0)&&(direction == 0)){
+            System.out.println("ENTRO AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 2");
+            return 2;
+        }else if((paddle.getX()-speedMove <= 0)&&(direction == 1)){
+            return 3;
+        }
+        if ((paddle.getX()+paddle.getWidth()+speedMove >= width)&&(direction == 2)){
+            return 0;
+        }else if ((paddle.getX()+paddle.getWidth()+speedMove >= width)&&(direction == 3)){
+            return 1;
+        }
+
+        if (((paddle.getY() + paddle.getHeight()) + speedMove >= height) && (direction == 1)) {
+            return 0;
+        } else if (((paddle.getY() + paddle.getHeight()) + speedMove >= height) && (direction == 3)) {
+            return 2;
+        }
+        return direction;
+    }
+    public boolean isChangeDirection() {
+        return changeDirection;
+    }
+
+    public void setChangeDirection(boolean changeDirection) {
+        this.changeDirection = changeDirection;
+    }
+
+    public Handler getControl() {
+        return control;
+    }
+
+    public void setControl(Handler control) {
+        this.control = control;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeigth(int height) {
+        this.height = height;
+    }
+
+    public int getSpeedMove() {
+        return speedMove;
+    }
+
+    public void setSpeedMove(int speedMove) {
+        this.speedMove = speedMove;
+    }
+
+    public ImageView getPaddle() {
+        return paddle;
+    }
+
+    public void setPaddle(ImageView paddle) {
+        this.paddle = paddle;
+    }
+}
---
+++ PONG/app/src/test/java/com/example/jorge/myapplication/BallThread.java
@@ -0,0 +1,94 @@
+package com.example.jorge.myapplication;
+
+import android.os.Handler;
+import android.widget.ImageView;
+
+
+/**
+ * Created by Jorge on 21/10/2017.
+ */
+
+public class BallThread implements Runnable {
+    MainActivity main;
+    ImageView ball;
+    int height, speedMove = 10;
+    boolean down, up;
+    Handler control;
+
+    public BallThread() {
+    }
+
+    public BallThread(MainActivity main) {
+        this.main = main;
+        down = up = false;
+        control = new Handler();
+    }
+
+    @Override
+    public void run() {
+        ball = main.getBall();
+        height = main.getBoard().getHeight();
+        if ((up && !down) && (!((ball.getY() - speedMove) <= 0))) {
+            ball.setY(ball.getY() - speedMove);
+        } else if ((!up && down) && (!((ball.getY() + ball.getHeight() + speedMove) >= (height)))) {
+            ball.setY(ball.getY() + speedMove);
+        }
+        control.postDelayed(this,(long)0.0005);
+    }
+
+    public MainActivity getMain() {
+        return main;
+    }
+
+    public void setMain(MainActivity main) {
+        this.main = main;
+    }
+
+    public ImageView getBall() {
+        return ball;
+    }
+
+    public void setBall(ImageView ball) {
+        this.ball = ball;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getSpeedMove() {
+        return speedMove;
+    }
+
+    public void setSpeedMove(int speedMove) {
+        this.speedMove = speedMove;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public Handler getControl() {
+        return control;
+    }
+
+    public void setControl(Handler control) {
+        this.control = control;
+    }
+}
